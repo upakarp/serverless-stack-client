a5ee15adb22931a10b6ff93544bebc098aa3b3bf
upakar <upakarpaudel96@gmail.com>
2020-12-03 13:14:57 -0700

    Updating the lander text

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -66,7 +66,7 @@ export default function Home() {
     return (
       <div className="lander">
         <h1>Scratch</h1>
-        <p className="text-muted">A simple note taking app</p>
+        <p className="text-muted">A simple note taking application</p>
         <div className="pt-3">
           <Link to="/login" className="btn btn-info btn-lg mr-3">
             Login
